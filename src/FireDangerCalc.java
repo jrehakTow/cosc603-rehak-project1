0e0a5bfbaa2d4f4b13d4c697eaf1cbc9944dbafe
jrehakTow <jrehak1@students.towson.edu>
2016-02-11 15:52:43 -0500

    update functions
    Add a b finders as well

--- src/FireDangerCalc.java
+++ src/FireDangerCalc.java
@@ -15,6 +15,7 @@ public class FireDangerCalc {
 	double PRECIP; //amount of precipitation in INCHES
 	double FFM; //fine Fuel moisture rating
 	double BUI; //Buildup Index
+	double BUO; //yesterday's BUI
 	double WIND; // wind speed MPH
 	double ADFM; //adjusted fuel moisture rating based on 50 day lag
 	double grass; //Fine fuel spread 
@@ -23,32 +24,52 @@ public class FireDangerCalc {
 	double a; //regression coefficient
 	double b; //regression coefficient 
 	
-	public static double FineFuelMoisture(double a, double b){
+	public void calcAB(double tempRange){
+		//all temperatures are measured in fahrenheit
+		if(tempRange < 4.5){
+			a = 30;
+			b = -0.1859;
+		}
+		else if(tempRange < 12.5){
+			a = 19;
+			b = -0.0859;
+		}
+		else if(tempRange < 27.5){
+			a = 13.8;
+			b = -0.0579;
+		}
+		else{
+			a = 22.5;
+			b = -0.0774;
+		}
+	}
+	
+	public double calcFineFuelMoisture(double a, double b){
 		double FFM = 0;
 		return FFM;
 	}
 	
-	public static double AdjustedFuelMoist(double FFM, double BUI){
+	public double calcAdjustedFuelMoist(double FFM, double BUI){
 		double ADFM = 0;
 		return ADFM;
 	}
 	
-	public static double BuildupIndex(double BUO, double PRECIP){
+	public double calcBuildupIndex(double BUO, double PRECIP){
 		double BUI = 0;
 		return BUI;
 	}
 	
-	public static double FineFuelSpread(double a, double b, double WIND){
+	public double calcFineFuelSpread(double a, double b, double WIND){
 		double grass = 0;
 		return grass;
 	}
 	
-	public static double TimberSpreadIndex(double a, double b, double WIND, double ADFM){
+	public double calcTimberSpreadIndex(double a, double b, double WIND, double ADFM){
 		double timber = 0;
 		return timber;
 	}
 	
-	public static double FireLoadIndex(){
+	public double calcFireLoadIndex(){
 		double FLOAD = 0;
 		return FLOAD;
 	}

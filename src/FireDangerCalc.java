139faf8576495836c0c5b5dcd1e037663e97fa1e
jrehakTow <jrehak1@students.towson.edu>
2016-02-11 14:38:12 -0500

    Functions
    Initial declaration of functions

--- src/FireDangerCalc.java
+++ src/FireDangerCalc.java
@@ -1,6 +1,39 @@
 package src;
 
 public class FireDangerCalc {
+	
+	boolean snow;
+	boolean rain;
+	
+	public static double FineFuelMoisture(double a, double b){
+		double FFM = 0;
+		return FFM;
+	}
+	
+	public static double AdjustedFuelMoist(double FFM, double BUI){
+		double ADFM = 0;
+		return ADFM;
+	}
+	
+	public static double BuildupIndex(double BUO, double PRECIP){
+		double BUI = 0;
+		return BUI;
+	}
+	
+	public static double FineFuelSpread(double a, double b, double WIND){
+		double grass = 0;
+		return grass;
+	}
+	
+	public static double TimberSpreadIndex(double a, double b, double wind, double ADFM){
+		double timber = 0;
+		return timber;
+	}
+	
+	public static double FireLoadIndex(){
+		double FLOAD = 0;
+		return FLOAD;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
